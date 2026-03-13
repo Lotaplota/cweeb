7b4d2fc169a7d005144212e6a76d76b092156a52
lotaplota <kristhian.lucas@gmail.com>
2026-03-13 16:50:55 -0300

    successfully downloads the pages to /images/

--- main.c
+++ main.c
@@ -2,47 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-// char * find(char filename[64], char initiator[64], char terminator[2])
-// {
-//     FILE * file = fopen(filename, "r");
-
-//     // Creating buffers for the current line and the selected substring
-//     char line[1024];
-//     char select[1024];
-
-//     while (fgets(line, sizeof(line), file) != NULL)
-//     {
-//         // Finding the position of the substring
-//         char * pos = strstr(line, initiator);
-
-//         if (pos == NULL) 
-//         {
-//             printf("Substring \"%s\" not found\n", initiator);
-//         }
-//         else
-//         {
-//             printf("Found: ");
-
-//             // Should start printing from the i-th character
-//             // i should be the length of the initiator substring minus 1 for the \0
-//             int i = sizeof(initiator) - 1; int j = 0;
-
-//             // Prints while the current character is not the chosen terminator
-//             while (pos[i] != terminator[0])
-//             {
-//                 select[j] = pos[i];
-//                 i++; j++;
-//             }
-//         }
-//     }
-// } // CONTINUE don't do separate functions, do everything in main and later separate it in functions
-
 void getHtmlData()
 {
     FILE * fw = fopen("content.txt", "w");
 
     // Requesting the URL from the user
-    char url[64] = "";
+    char url[64];
     printf("insert the desired url\nhttp://");
     scanf("%s", url);
 
@@ -50,7 +15,6 @@ void getHtmlData()
     char argument[128] = "curl -o content.txt https://"; // won't run without the s in https FOR SOME REASON
     strcat(argument, url);
 
-    // 
     system(argument);
     
     fclose(fw);
@@ -58,13 +22,11 @@ void getHtmlData()
 
 void empty(char * a, int size)
 {
-    memset(a, 0, size * (sizeof a[0]) );
+    memset(a, 0, size * (sizeof a[0]));
 }
 
-int main(void)
+void separateLinks()
 {
-    getHtmlData();
-
     FILE * html = fopen("content.txt", "r");
     FILE * links = fopen("links.txt", "w");
 
@@ -78,8 +40,6 @@ int main(void)
 
         if (pos != NULL)
         {
-            printf("Found: ");
-
             // Should start printing from the i-th character
             // i should be the length of the initiator substring minus 1 for the \0
             int i = sizeof("ratio-content\" src=\"") - 1; int j = 0;
@@ -94,13 +54,36 @@ int main(void)
             // Saving the extracted link found to the file
             fprintf(links,"%s\n", select);
         }
-        else
-        {
-            printf("Error!\n");
-        }
 
         // Emptying the array for the next link
         empty(select, 1024);
     }
+
+    fclose(html); fclose(links);
 }
 
+void downloadImages()
+{
+    system("mkdir images");
+    FILE * links = fopen("links.txt", "r");
+
+    // Creating page counter to be appended to the filenames
+    int pageNum = 1;
+    char buff[1024] = "";
+    
+    while (fgets(buff, sizeof(buff), links) != NULL)
+    {
+        char command[64];
+        sprintf(command, "curl -o images/page%02i.png %s", pageNum, buff);
+
+        system(command);
+        pageNum++;
+    }
+}
+
+int main(void)
+{
+    getHtmlData();
+    separateLinks();
+    downloadImages();
+}
--- tests/download.c
+++ tests/download.c
@@ -3,5 +3,5 @@
 
 int main(void)
 {
-    system("curl -o testImg.png https://cdn.onepiecechapters.com/file/CDN-M-A-N/op_1176_nn_011.png");
+    system("curl -o test-folder/testImg.png https://cdn.onepiecechapters.com/file/CDN-M-A-N/op_1176_nn_011.png");
 }
